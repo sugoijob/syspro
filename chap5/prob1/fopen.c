8aeb3ba6afc3fcbff040ea57d0f30e5c97e9ac08
sugoijob <jearim0927@naver.com>
2024-10-10 02:50:19 +0000

    2024 10 10 silp seup jung stop zz

---
+++ chap5/prob1/fopen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unisted.h>
+#include <fcntl.h>
+
+int main(int argc, char *argv[])
+{
+	int fd;
+	
+	if ((fd = open(argv[1], O_RDWR))== -1)
+		printf("File Open Error\n");
+	else printf("file %s Open Sucess : %d\n", argv[1], fd);
+
+	close(fd);
+
+	exit(0);
+}
+
