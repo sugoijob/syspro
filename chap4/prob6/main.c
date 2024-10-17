3bdb24b7fc1cc8c6275e8cd87011bdf2b164ac62
sugoijob <jearim0927@naver.com>
2024-10-17 02:52:59 +0000

    silseup done nice zz

---
+++ chap4/prob6/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "student.h"
+
+int main(int argc, char* argv[])
+{
+	struct student rec;
+	FILE *fp;
+
+	if(argc != 2)
+	{
+		fprintf(stderr, "how to use : %s filename\n", argv[0]);
+		exit(1);
+	}
+	fp = fopen(argv[1], "wb");
+	printf("%-9s %-7s %-4s\n", "StudentID", "Name", "Score");
+	while(scanf("%d %s %d", &rec.id , rec.name, &rec.score)==3)
+		fwrite(&rec, sizeof(rec), 1, fp);
+	
+	fclose(fp);
+	exit(0);
+}
+
+
